e9ab542c4adacd8ef455a0a36fc0d5077cb8edec
Hosam <hosamien@gmail.com>
2021-12-10 20:51:24 +0100

    Add - errorPage

---
+++ src/components/ErrorPage.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let message
+</script>
+
+<h3>{message}</h3>
+
+
+
+<style>
+    h3{
+        color: red;
+        font-weight: bold;
+        font-size: xx-large;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%)
+    }
+</style>
