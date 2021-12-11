33a61aa81805f295cdb0dae34b8657409724d9f3
xavierLINET <xavier.linet@student.vinci.be>
2021-12-11 19:55:41 +0100

    bannir fini

--- src/routes/ban/index.svelte
+++ src/routes/ban/index.svelte
@@ -1,37 +1,48 @@
 <script>
-    import "../../styles/tailwind-output.css";
-    import Member from "../../components/MemberLine.svelte";
-    import Navbar from "../../components/Navbar.svelte";
-    import { onMount } from 'svelte';
+	import '../../styles/tailwind-output.css';
+	import Member from '../../components/MemberLine.svelte';
+	import Navbar from '../../components/Navbar.svelte';
+	import { onMount } from 'svelte';
 
-    let membersArray = []
-    let token
-    let emailSearched = ""
+	let membersArray = []
+    let bannedMembers = []
     let filteredMembers = []
-    let filtered = false
+    //sert de base pour les tris de string
+    let baseMembers = []
 
-    let USER;
-    onMount(() => {
-        USER = JSON.parse(localStorage.getItem('user'))
-        token = USER.token
-    });
+	let token;
+	let emailSearched = ''
+	let filtered = false
+    let banned = false;
 
-    $: {
-        console.log(`token ${token}`);
-	    let promise = fetchMembers();
-        promise.then((result) => {
-            console.log(result)
-            membersArray = result.filter(m => new Date(m.banni).valueOf() < Date.now()).sort((a, b) => a.email.localeCompare(b.email)) 
-        }).catch(err=>console.log(err))
-    }
+	let USER;
+	onMount(() => {
+		USER = JSON.parse(localStorage.getItem('user'));
+		token = USER.token;
+	});
 
-    async function fetchMembers() {
-        if (token == undefined) return []; 
-		const response = await fetch('https://pfe-backend1.herokuapp.com/Members',{
-            headers: {"Content-Type" :"application/json", 
-                Authorization: token },
-            method:'GET',
-        });
+	$: {
+		console.log(`token `);
+		let promise = fetchMembers();
+		promise
+			.then((result) => {
+				baseMembers = membersArray = result
+					.filter((m) => new Date(m.banni).valueOf() < Date.now())
+                    .filter((m) => m.id != USER.id)
+					.sort((a, b) => a.email.localeCompare(b.email));
+                bannedMembers = result
+                    .filter((m) => new Date(m.banni).valueOf() > Date.now())
+                    .sort((a, b) => a.email.localeCompare(b.email));
+			})
+			.catch((err) => console.log(err));
+	}
+
+	async function fetchMembers() {
+		if (token == undefined) return [];
+		const response = await fetch('https://pfe-backend1.herokuapp.com/Members', {
+			headers: { 'Content-Type': 'application/json', Authorization: token },
+			method: 'GET'
+		});
 		const resp = await response.json();
 
 		if (response.ok) {
@@ -41,48 +52,61 @@
 		}
 	}
 
-    function handleInput(e){
-        //console.log(membersArray)
-        emailSearched = e.target.value
-        filteredMembers = membersArray.filter((m) => m.email.startsWith(emailSearched))
-        filtered = true
-    }
+	function handleInput(e) {
+		emailSearched = e.target.value;
+		filteredMembers = baseMembers.filter((m) => m.email.startsWith(emailSearched));
+		filtered = true;
+	}
 
+	function handleCheck(e) {
+		if (e.target.checked) {
+            baseMembers = bannedMembers
+            banned = true
+        }
+		else {
+            baseMembers = membersArray
+            banned = false
+        }
+        filteredMembers = baseMembers
+        if (emailSearched != "") filteredMembers = baseMembers.filter((m) => m.email.startsWith(emailSearched));
+	}
 </script>
 
-
 <main>
-    <Navbar />
-    <div class="min-h-full flex items-center justify-center py-12 px-15 sm:px-6 lg:px-8">
-        <div class="max-w-4xl w-full space-y-8">
-            <div>
-                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Voici l'ensemble des utilisateurs:</h2>
-            </div>
-            <div class="rounded-md shadow-sm -space-y-px">
-                <div class="max-w-sm">
-                    <label for="email-address" class="sr-only">Email address</label>
-                    <input 
-                        name="email" 
-                        on:input={handleInput} 
-                        placeholder="Rechercher un email" 
-                        class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                        
-                    />
-                </div>
-            </div>
-            <div>
-                {#if filteredMembers.length != 0}
-                    {#each filteredMembers as member}
-                        <Member email={member.email} id={member.id} token={token} />
-                    {/each}
-                {:else if filtered}
-                    <p class="has-text-centered">Désolé personne ne correspond à votre recherche</p>
-                {:else}
-                    {#each membersArray as member}
-                        <Member email={member.email} id={member.id} token={token} />
-                    {/each}
-                {/if}
-            </div>
-        </div>
-    </div>
+	<Navbar />
+	<div class="min-h-full flex items-center justify-center py-12 px-15 sm:px-6 lg:px-8">
+		<div class="max-w-4xl w-full space-y-8">
+			<div>
+				<h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+					Voici l'ensemble des utilisateurs:
+				</h2>
+			</div>
+			<div class="rounded-md shadow-sm -space-y-px">
+				<div class="max-w-sm">
+					<label for="email-address" class="sr-only">Email address</label>
+					<input
+						name="email"
+						on:input={handleInput}
+						placeholder="Rechercher un email"
+						class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+					/>
+					<input type="checkbox" name="seeBan" on:change={handleCheck} />
+					<span>Cliquez pour voir les bannis</span>
+				</div>
+			</div>
+			<div>
+				{#if filteredMembers.length != 0}
+					{#each filteredMembers as member}
+						<Member email={member.email} id={member.id} token={token} banned={banned} banDate={member.banni.substring(0, 10)} />
+					{/each}
+				{:else if filtered}
+					<p class="has-text-centered">Désolé personne ne correspond à votre recherche</p>
+				{:else}
+					{#each membersArray as member}
+						<Member email={member.email} id={member.id} token={token} banned={banned} banDate={member.banni} />
+					{/each}
+				{/if}
+			</div>
+		</div>
+	</div>
 </main>
