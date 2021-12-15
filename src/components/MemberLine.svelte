ea1256d36e9f0bb0c7d16dafe2c533fc392cdade
erkutkoc <71339143+erkutkoc@users.noreply.github.com>
2021-12-15 19:01:39 +0100

    fix - ban duration

--- src/components/MemberLine.svelte
+++ src/components/MemberLine.svelte
@@ -8,7 +8,7 @@
 
 	import UserServices from '../services/userServices.js';
 	import '../styles/tailwind-output.css';
-    import colors from 'svelte-materialify/src/utils/colors';
+	import colors from 'svelte-materialify/src/utils/colors';
 	//import 'bootstrap/dist/css/bootstrap.min.css';
 
 	let banDateY;
@@ -25,7 +25,11 @@
 
 	const ban = (e, duration) => {
 		if (duration == 0) e.target.innerText = 'OK';
-		else{ if(duration == 1) e.target.innerText = 'Banni pour 1 jour'; else if(duration == -1) e.target.innerText = 'Banni définitivement';}
+		else {
+			if (duration == 1) e.target.innerText = 'Banni pour 1 jour';
+			else if (duration == -1) e.target.innerText = 'Banni définitivement';
+			else e.target.innerText = "Banni pour "+duration+ " jours";
+		}
 		banUser(token, id, duration);
 	};
 
@@ -49,23 +53,33 @@
 	<td style="">{email}</td>
 	<td style="">
 		<div class="is-pulled-right">
-            {#if !admin}
-			{#if banned}
-				<button class="button deep-purple accent-3" disabled> Banni jusqu'au {bandDateFormat}</button>
-				<button on:click={(e) => ban(e, 0)} class="button blue darken-1">Supprimer le ban </button>
-			{:else}
-				<button on:click={(e) => ban(e, 1)} class="button red darken-1">Bannir 1 jour </button>
-
-				<button on:click={(e) => ban(e, 30)} class="button red darken-3">Bannir 1 mois </button>
-
-				<button on:click={(e) => ban(e, -1)} class="button red darken-4">Bannir définitivement </button>
-
-				{#if admin}
-					<button on:click={(e) => makeAdmin(e)} class="button blue darken-2" disabled>Déjà Admin </button>
+			{#if !admin}
+				{#if banned}
+					<button class="button deep-purple accent-3" disabled>
+						Banni jusqu'au {bandDateFormat}</button
+					>
+					<button on:click={(e) => ban(e, 0)} class="button blue darken-1"
+						>Supprimer le ban
+					</button>
 				{:else}
-					<button on:click={(e) => makeAdmin(e)} class="button green lighten-2">Rendre administrateur </button>
-				{/if}
-			{/if}{:else}<p class="red-text darken-1">Modification non autorisée !</p>{/if}
+					<button on:click={(e) => ban(e, 1)} class="button red darken-1">Bannir 1 jour </button>
+
+					<button on:click={(e) => ban(e, 30)} class="button red darken-3">Bannir 1 mois </button>
+
+					<button on:click={(e) => ban(e, -1)} class="button red darken-4"
+						>Bannir définitivement
+					</button>
+
+					{#if admin}
+						<button on:click={(e) => makeAdmin(e)} class="button blue darken-2" disabled
+							>Déjà Admin
+						</button>
+					{:else}
+						<button on:click={(e) => makeAdmin(e)} class="button green lighten-2"
+							>Rendre administrateur
+						</button>
+					{/if}
+				{/if}{:else}<p class="red-text darken-1">Modification non autorisée !</p>{/if}
 		</div></td
 	>
 </tr>
@@ -73,6 +87,6 @@
 <style>
 	button {
 		width: 200px;
-        color:white;
+		color: white;
 	}
 </style>
