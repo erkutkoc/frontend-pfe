8929f2850cb0be81202ad050030493c54967d4c3
Hosam <hosamien@gmail.com>
2021-12-15 06:03:44 +0100

    fix layout form in edit annonce

--- src/routes/[id]/edit.svelte
+++ src/routes/[id]/edit.svelte
@@ -112,7 +112,8 @@
 					class="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-1"
 				>
 					<div>
-						<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                        <dt class="font-medium text-gray-900">Titre</dt>
+						<h2 class="text-3xl  tracking-tight text-gray-900 sm:text-4xl">
 							<input
 								class="input is-normal"
 								type="text"
@@ -121,8 +122,8 @@
 								required
 							/>
 						</h2>
-
-						<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl" id="prix">
+                        <dt class="font-medium text-gray-900">Prix</dt>
+						<h2 class="text-3xl  tracking-tight text-gray-900 sm:text-4xl" >
 							<input
 								class="input is-normal"
 								type="number"
